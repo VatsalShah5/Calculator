949efad522dbf49e385ea6a4e9549215b3d1ff7e
Vatsal Shah <Vatsalshah4567@gmail.com>
2023-08-20 20:53:51 +0530

    now public

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,13 +1,22 @@
 let string = "";
+let dotUsed = false; // Flag to track if the dot has been used
+
 let buttons = document.querySelectorAll('button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
             string = eval(string);
             document.querySelector('input').value = string;
+        } else if (e.target.innerHTML === 'AC') {
+            string = "";
+            dotUsed = false; // Reset the dot usage flag
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML === '<sup class="green">+</sup>/-') {
+            string = "-";
+            document.querySelector('input').value = string;
+        } else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
         }
-        console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
-    })
-})
+    });
+});
